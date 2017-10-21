478507e0d0ca27e2002643efd2e386f3e0b2dbdc
Ian Lyttle <ian.lyttle@schneider-electric.com>
2017-10-21 13:25:30 -0500

    gets docompose written, but not yet tested

---
+++ R/decompose.R
@@ -0,0 +1,93 @@
+#' Decompose a date into components
+#'
+#' TODO - better names for these functions
+#'
+#' One is used to operate on a vector, the other to operate on a data frame.
+#'
+#' Talk about ISO weeks, talk about using years.
+#'
+#' @param date        `Date`, date to decompose
+#' @param week_style  `character`, style to use: `"nam"`, `"iso"`
+#' @param use_year    `logical`, use `year` as a variable
+#'
+#' @return [decompose_date]:
+#' `tbl_df` with variables:
+#' \describe{
+#'   \item{.year}{`integer`, year to which the week belongs}
+#'   \item{.week}{`integer`, number of week}
+#'   \item{.wday}{`integer`, identifies the day in the week}
+#' }
+#' @export
+#'
+decompose_date <- function(date, week_style = "nam", use_year = TRUE) {
+
+}
+
+.decompose_date_nam <- function(date) {
+
+  date <- as.Date(date)
+
+  tibble::tibble(
+    .year = lubridate::year(date),
+    .week = lubridate::week(date),
+    .wday = lubridate::wday(date)
+  )
+
+}
+
+.decompose_date_iso <- function(date) {
+
+  date <- as.Date(date)
+
+  dec_nam <- .decompose_date_nam(date)
+
+  # convert nam weekday to iso weeksay
+  .wday_iso <- .wday_nam_to_iso(dec_nam$.wday)
+
+  .week_iso <- lubridate::isoweek(date)
+
+  # adjust year, if needed
+  .year_iso <-
+    dplyr::case_when(
+      # week belongs to following iso year
+      (.week_iso < 3L) & (dec_nam$.week > 48L) ~ dec_nam$.year + 1L,
+      # week belongs to previous iso year
+      (.week_iso > 48L) & (dec_nam$.week < 3L) ~ dec_nam$.year - 1L,
+      # iso week and nam week "agree"
+      TRUE ~ dec_nam$.year
+    )
+
+  tibble::tibble(.year = .year_iso, .week = .week_iso, .wday = .wday_iso)
+}
+
+.wday_iso_to_nam <- function(x) {
+  # key is implied in the index - this is the iso weekday (1-7)
+  # value is the value of the nam weekday (1-7)
+  key_value <- c(2L, 3L, 4L, 5L, 6L, 7L, 1L)
+
+  key_value[x]
+}
+
+.wday_nam_to_iso <- function(x) {
+  # key is implied in the index - this is the nam weekday (1-7)
+  # value is the value of the iso weekday (1-7)
+  key_value <- c(7L, 1L, 2L, 3L, 4L, 5L, 6L)
+
+  key_value[x]
+}
+
+
+#' @rdname decompose_date
+#'
+#' @return [mutate_ggocto()]:
+#' `.data`, with additional columns generated using [decompose_date()]
+#' @export
+#'
+mutate_ggocto <- function(.data, date = .data$date, week_style = "nam",
+                          use_year = TRUE) {
+
+  decompose <-
+    decompose_date(date, week_style = week_style, use_year = use_year)
+
+  dplyr::bind_cols(.data, decompose)
+}
